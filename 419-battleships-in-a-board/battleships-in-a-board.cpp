c9f1a1d4d2421ec35a44ec45e1161e1d2a216d5f
Kaviya Ramasamy <155222409+kaviyar23cse@users.noreply.github.com>
2025-12-18 09:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (96.51%) - LeetSync

---
+++ 419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    void dfs(vector<vector<char>> &grid,int r,int c){
+        if(r<0||c<0||r>=grid.size()||c>=grid[0].size()){
+            return;
+        }
+        if(grid[r][c]=='X'){
+            grid[r][c]='.';
+            dfs(grid,r+1,c);
+            dfs(grid,r-1,c);
+            dfs(grid,r,c+1);
+            dfs(grid,r,c-1);
+        }
+    }
+    int countBattleships(vector<vector<char>>& grid) {
+        int count=0;
+        for(int r=0;r<grid.size();r++){
+            for(int c=0;c<grid[0].size();c++){
+                if(grid[r][c]=='X'){
+                    dfs(grid,r,c);
+                    count++;
+                }
+            }
+        }
+            return count;
+        
+
+        
+    }
+};
